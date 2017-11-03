14369c49f648e0cc4b1c1125044ebefb3da5368f
Andres Abrego <andres.r.abrego@gmail.com>
2017-11-03 11:14:02 -0500

    Added classes from UML Updated gitignore to ignore IDEA files

---
+++ Driver.java
@@ -0,0 +1,38 @@
+
+public class Driver {
+
+    private String name;
+    private long phoneNumber;
+
+    private Race assignedRace;
+
+    Driver() {
+
+    }
+
+    Driver(String name) {
+
+    }
+
+    Driver(String name, long number) {
+
+    }
+
+    String getName() {
+
+        return name;
+    }
+
+    void setName(String name) {
+
+    }
+
+    long getPhoneNumber() {
+
+        return phoneNumber;
+    }
+
+    void setPhoneNumber(long number) {
+
+    }
+}
---
+++ Race.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+
+public class Race {
+
+    private Date startTime;
+
+    private Driver[] drivers;
+
+    Race() {
+
+    }
+
+    Race(Date startTime) {
+
+    }
+
+    Date getStartTime() {
+
+        return startTime;
+    }
+
+    void setStartTime(Date time) {
+
+    }
+}
