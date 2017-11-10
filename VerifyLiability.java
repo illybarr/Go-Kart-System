be22f95408acfd4ce2ea9a2dac80a82d19211939
illybarr <33291685+illybarr@users.noreply.github.com>
2017-11-10 11:27:56 -0600

    Update VerifyLiability.java

--- VerifyLiability.java
+++ VerifyLiability.java
@@ -1,28 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author barrerai9
- */
 import java.util.Scanner;
+import java.io.*;
 
 public class VerifyLiability {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
         
         String inputAnswer;
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Read this Liability Form:");
+        System.out.println("Drivers must read this Liability Form:");
         
         System.out.println("       I HAVE READ THIS RELEASE AND WAIVER OF LIABILITY, ASSUMPTION OF RISK AND\n" +
                            "       INDEMNITY AGREEMENT, AND UNDERSTAND THAT BY SIGNING IT I GIVE UP SUBSTANTIAL\n" +
@@ -32,12 +23,22 @@ public class VerifyLiability {
                            "       AGREEMENT TO BE A COMPLETE AND UNCONDITIONAL RELEASE OF ALL LIABILITY TO THE\n" +
                            "       GREATEST EXTENT ALLOWED BY LAW. ");
         
-        System.out.println("Do you agree to accept all liability? Types 'yes' or 'no':");
+        System.out.println("Have the drivers accepted liability? Types 'yes' or 'no':");
         
         inputAnswer = sc.nextLine();
         
         if ("yes".equals(inputAnswer))
-            System.out.println("You can be assigned to a go-kart.");
+        {
+            System.out.println("\nHere are all the people who can drive: ");
+            //change the path to wherever the textfile is located 
+            FileReader readfile =
+            new FileReader("C:\\Users\\barrerai9\\Documents\\NetBeansProjects\\GoKartDemo\\src\\ClientInformation.txt");
+ 
+             int i;
+             while ((i=readfile.read()) != -1) 
+             System.out.print((char) i);
+            
+        }
         else
         {
             System.out.println("You cannot drive a go-kart.");
