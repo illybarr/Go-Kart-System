24d9e5b583f83279cd4233566ae6483d95fe7327
Andres Abrego <andres.r.abrego@gmail.com>
2017-11-12 20:35:00 -0600

    Moved code to /src directory Updated .gitignore

---
+++ src/Employee.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author barrerai9
+ */
+
+public class Employee {
+    
+    String firstName, lastName;
+    int phoneNumber;
+    
+    public Employee(String firstName, String lastName, int phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+  
+}
