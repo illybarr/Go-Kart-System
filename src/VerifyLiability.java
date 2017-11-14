2b64ac8da63684a707d633017ee6d60167bc3831
illybarr <33291685+illybarr@users.noreply.github.com>
2017-11-14 14:35:37 -0600

    Update VerifyLiability.java

--- src/VerifyLiability.java
+++ src/VerifyLiability.java
@@ -1,19 +1,22 @@
+import java.io.File;
 import java.util.Scanner;
-import java.io.*;
+
 
 public class VerifyLiability {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         
         String inputAnswer;
+        int inputDrivers;
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Drivers must read this Liability Form:");
+        System.out.println("Drivers must read this Liability Form first:");
         
         System.out.println("       I HAVE READ THIS RELEASE AND WAIVER OF LIABILITY, ASSUMPTION OF RISK AND\n" +
                            "       INDEMNITY AGREEMENT, AND UNDERSTAND THAT BY SIGNING IT I GIVE UP SUBSTANTIAL\n" +
@@ -23,27 +26,37 @@ public class VerifyLiability {
                            "       AGREEMENT TO BE A COMPLETE AND UNCONDITIONAL RELEASE OF ALL LIABILITY TO THE\n" +
                            "       GREATEST EXTENT ALLOWED BY LAW. ");
         
-        System.out.println("Have the drivers accepted liability? Types 'yes' or 'no':");
-        
+        //answers must be written exactly like explained or it wont run
+        System.out.println("Have all the drivers accepted liability? Types 'yes' or 'no':");
         inputAnswer = sc.nextLine();
         
         if ("yes".equals(inputAnswer))
         {
-            System.out.println("\nHere are all the people who can drive: ");
+            System.out.println("How many total people are driving go-karts?");
+            inputDrivers = sc.nextInt();
           
-            //change the path to wherever the textfile is located 
-            //this code is temporary and will be changed
-            FileReader readfile =
-            new FileReader("/ClientInformation.txt");
+            //change the path to wherever the textfile is located on YOUR computer
+            Scanner readFile = new Scanner(new File("C:\\Users\\barrerai9\\Documents\\NetBeansProjects\\Go-Kart System\\src\\ClientInformation.txt"));
  
-             int i;
-             while ((i=readfile.read()) != -1) 
-             System.out.print((char) i);
-            
+             int count = 0;
+             String[] values = new String[12];
+             
+             while(readFile.hasNext())
+             {
+                 values[count] = readFile.nextLine();
+                 count++;
+             }
+             
+            System.out.println("Here is the customer information \nof the customers who can drive:");
+            System.out.println("---------------------------------------");
+            for(int x=0; x<inputDrivers; x++)
+            {
+                System.out.println(values[x]);
+            }
         }
         else
         {
-            System.out.println("You cannot drive a go-kart.");
+            System.out.println("They cannot drive a go-kart.");
             System.exit(0);
         }
            
