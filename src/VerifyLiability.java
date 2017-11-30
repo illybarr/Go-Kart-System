9829b3f0671e57218711e1ecc7dfebc1dbbf21b0
illybarr <33291685+illybarr@users.noreply.github.com>
2017-11-30 12:52:13 -0600

    Update VerifyLiability.java

--- src/VerifyLiability.java
+++ src/VerifyLiability.java
@@ -1,21 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.IOException;
 
 //this method cannot run until implemented in the main class in Client
 
 public class VerifyLiability {
 
-    public void verifyLiability() throws FileNotFoundException
+    public void verifyLiability() throws FileNotFoundException, IOException
     {
     	String inputAnswer;
-        int inputDrivers;
-
-        Employee kart1 = new Employee();
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Drivers must read this Liability Form first:");
+        System.out.println("\nDrivers must read this Liability Form first:");
         
         System.out.println("       I HAVE READ THIS RELEASE AND WAIVER OF LIABILITY, ASSUMPTION OF RISK AND\n" +
                            "       INDEMNITY AGREEMENT, AND UNDERSTAND THAT BY SIGNING IT I GIVE UP SUBSTANTIAL\n" +
@@ -26,43 +24,14 @@ public class VerifyLiability {
                            "       GREATEST EXTENT ALLOWED BY LAW. ");
         
         //answers must be written exactly like explained or it wont run
-        System.out.println("Have all the drivers accepted liability? Types 'yes' or 'no':");
+       System.out.println("Have all the drivers accepted liability? Types 'yes' or 'no':");
         inputAnswer = sc.nextLine();
         
         if ("yes".equals(inputAnswer))
         {
-            System.out.println("How many total people are driving go-karts?");
-            inputDrivers = sc.nextInt();
+            Driver readDatabase = new Driver();
+            readDatabase.readExistingMembers();
             
-          
-            //change the path to wherever the textfile is located on YOUR computer
-            Scanner readFile = new Scanner(new File("C:\\Users\\Iliana\\Music\\Go-Kart-System-master\\src\\ClientInformation.txt"));
- 
-             int count = 0;
-             int kartCount = 1;
-             String[] values = new String[12];
-             
-             while(readFile.hasNext())
-             {
-                 values[count] = readFile.nextLine();
-                 count++;
-             }
-             
-            System.out.println("Here is the customer information \nof the customers who can drive ");
-            System.out.println("and the karts assigned to each:");
-            System.out.println("---------------------------------------");
-            for(int x=0; x<inputDrivers; x++)
-            {
-                System.out.println(values[x] + " - #" + kartCount);
-                kartCount++;
-            }
-            
-            kart1.scheduleRace();			//this should be implemented in the main class of the Server
-            									//so it can be called by the Client
-            
-            
-            
-            readFile.close();
         }
         else
         {
